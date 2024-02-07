27f0cc2e6a22009688e94fdb7d9bd215413a990d
roguexsubmarine <roguexsubmarine@gmail.com>
2024-02-08 00:33:07 +0530

    added login page

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+from app import app
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return render_template("addquestion.html")
+    return render_template("login.html", error=error)
+
+
+@app.route("/akinator")
+def akinator():
+    return redirect("https://en.akinator.com/theme-selection")
+
+@app.route("/facinator")
+def facinator():
+    return render_template("facinator.html")
+
+@app.route("/reset")
+def reset():
+    return render_template("index.html")
+
+@app.route("/facinator/submit")
+def submit():
+    return render_template("submit.html")
+
+@app.route("/addnew")
+def addnew():
+    return render_template("addquestion.html")
