4565bbff8a574468092f086a94880cf87fea14ed
roguexsubmarine <roguexsubmarine@gmail.com>
2024-03-04 16:36:48 +0530

    integrated table yay flask runflask runflask runflask run

--- app.py
+++ app.py
@@ -3,6 +3,33 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from app import app
+import sqlite3
+con = sqlite3.connect("college_faculty.db")
+c = con.cursor()
+
+ #Execute a query to fetch faculties along with their department and subject details
+c.execute('''SELECT f.id, f.name AS faculty_name, d.name AS department_name, s.name AS subject_name, f.email
+                  FROM faculties f
+                  JOIN departments d ON f.department_id = d.id
+                  JOIN subjects s ON f.subject_id = s.id
+                  ''')
+
+item = c.fetchall()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route("/")
 def index():
@@ -57,5 +84,5 @@ def page_not_found(e):
 
 @app.route("/temp")
 def temp():
-    return render_template("layout2.html")
+    return render_template("layout2.html", items=item)
 
