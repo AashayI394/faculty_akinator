ecc6854b38e157e760d42a3c33e99877adbad088
AashayI394 <aashaysinamdar@gmail.com>
2024-03-27 14:56:14 +0530

    completed guess logic

--- guesslogic.py
+++ guesslogic.py
@@ -32,19 +32,7 @@ for sub in subs:
     yos.append(sub[0])
     yos_temp = yos
 
-
-
-# cursor.execute("SELECT DISTINCT semester FROM admindb")
-# subs = cursor.fetchall()
-# semester = []
-# for sub in subs:
-#     semester.append(sub[0])
-#     semester_temp = semester
-
 semester_temp = [1,2,3,4,5,6,7,8]
-
-
-
 
 cursor.execute("SELECT DISTINCT gender FROM admindb")
 subs = cursor.fetchall()
@@ -53,16 +41,12 @@ for sub in subs:
     gender.append(sub[0])
     gender_temp = gender
 
-
-
 cursor.execute("SELECT DISTINCT office FROM admindb")
 subs= cursor.fetchall()
 office = []
 for sub in subs:
     office.append(sub[0])
     office_temp = office
-
-
 
 cursor.execute("SELECT DISTINCT doctorate FROM admindb")
 subs = cursor.fetchall()
@@ -78,40 +62,32 @@ for sub in subs:
     subject.append(sub[0])
     subject_temp = subject
 
-
-
 def query_all():
-	conn = sqlite3.connect('facinator.db')
-	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM admindb")
-	result = cursor.fetchall()
-	return result
-	conn.close()
+    conn = sqlite3.connect('facinator.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM admindb")
+    result = cursor.fetchall()
+    return result
+    conn.close()
 
 def singlequery(column, res):
-	conn = sqlite3.connect('facinator.db')
-	cursor = conn.cursor()
-	query= f"SELECT * FROM admindb WHERE {column} = ?"
-	cursor.execute(query,(res,))
-	result = cursor.fetchall()
-	conn.close()
-	return result
-
+    conn = sqlite3.connect('facinator.db')
+    cursor = conn.cursor()
+    query= f"SELECT * FROM admindb WHERE {column} = ?"
+    cursor.execute(query,(res,))
+    result = cursor.fetchall()
+    conn.close()
+    return result
 
 def generate_random_number(length):
     if length < 1:
         raise ValueError("Length must be at least 1")
     return random.randint(0, length - 1)
 
-
 def find_intersection(list1, list2):
-    # Convert lists to sets for easy intersection calculation
     set1 = set(list1)
     set2 = set(list2)
-   
-    # Calculate intersection using the '&' operator
     intersection = list(set1 & set2)
-    
     return intersection
 
 def query_subjects1(years):
@@ -124,139 +100,56 @@ def query_subjects2(sem):
     subjects = cursor.fetchall()
     return [subject[0] for subject in subjects]
 
+def create_query(iteration):
+    n = generate_random_number(len(col_header) - 1)
+    col = col_header[n]
+    res = ""
+    rem_office = set()  # initialize
 
-def create_query():
-	n = generate_random_number(len(col_header)-1)
-	col= col_header[n]
-	res=""
-	match col:
-		case "department_name":
-			i = generate_random_number(len(dept_temp)-1)
-			res = dept_temp[i]
-			dept_temp.pop(i)
-			col_header.pop(n)
-		case "subject_name":
-			
-			years = yos_temp
-			subject_temp[:] = query_subjects1(years)
-			
-			sem = semester_temp
-			subject_temp[:] = query_subjects2(sem)
-			i = generate_random_number(len(subject_temp)-1)
-			res = subject_temp[i]
-			subject_temp.pop(i)
-		case "gender":
-			i = generate_random_number(len(gender_temp)-1)
-			res = gender_temp[i]
-			gender_temp.pop(i)
-			col_header.pop(n)
-		case "office":
-			i = generate_random_number(len(office_temp)-1)
-			res = office_temp[i]
-			office_temp.pop(i)
-			col_header.pop(n)
-		case "doctorate":
-			i = generate_random_number(len(doc_temp)-1)
-			res = doc_temp[i]
-			doc_temp.pop(i)
-			col_header.pop(n)
-		case "year_of_study":
-			i = generate_random_number(len(yos_temp)-1)
-			res = yos_temp[i]
-			yos_temp.pop(i)
-			#filter semesters based on the year selected
-			semester_temp[:] = [num for num in semester_temp if num==int(res)*2 or num == int(res)*2 -1]
-			col_header.pop(n)
-		case "semester":
-			i = generate_random_number(len(semester_temp)-1)
-			res = semester_temp[i]
-			semester_temp.pop(i)
-			#filter year based on the semester selected
-			yos_temp[:] = [num for num in yos_temp if num==ceil(float(res)/2) ]
-			col_header.pop(n)
-	return [col,res]
+    # Check if the last tuple's last element in result_list is true
+        # Your block of code here
+    match col:
+        case "department_name":
+            # Check if any office location has been popped
+            if len(office) != len(office_temp):
+                rem_office = set(office) - set(office_temp)
+            if rem_office:
+                placeholders = ",".join(["?"] * len(rem_office))
+                query = f"SELECT DISTINCT department_name FROM admindb WHERE office IN ({placeholders})"
+                cursor.execute(query, tuple(rem_office))
+                subs = cursor.fetchall()
+                dept_temp[:] = [sub[0] for sub in subs if sub[0] in dept_temp]
+            i = generate_random_number(len(dept_temp) - 1)
+            res = dept_temp[i]
+            col_header.pop(n)
+        case "subject_name":
+            years = yos_temp
+            subject_temp[:] = query_subjects1(years)
+            sem = semester_temp
+            subject_temp[:] = query_subjects2(sem)
+            i = generate_random_number(len(subject_temp) - 1)
+            res = subject_temp[i]
+            col_header.pop(n)
+        case "gender":
+            i = generate_random_number(len(gender_temp) - 1)
+            res = gender_temp[i]
+            col_header.pop(n)
+        case "office":
+            i = generate_random_number(len(office_temp) - 1)
+            res = office_temp[i]
+            col_header.pop(n)
+        case "doctorate":
+            i = generate_random_number(len(doc_temp) - 1)
+            res = doc_temp[i]
+            col_header.pop(n)
+        case "year_of_study":
+            i = generate_random_number(len(yos_temp) - 1)
+            res = yos_temp[i]
+            col_header.pop(n)
+        case "semester":
+            i = generate_random_number(len(semester_temp) - 1)
+            res = semester_temp[i]
+            col_header.pop(n)
 
+    return [col, res]
 
-
-# result = query_all()
-# print("obtained whole list")
-
-# col1 = "department_name"
-# res1 = "Computer Engineering"
-# ans = singlequery(col1,res1) 
-# print(ans)
-# print()
-# new_result = [t for t in result if t in ans]
-# result = new_result
-# print(result)
-
-
-# print("\n\n")
-
-# col2 = "gender"
-# res2 = "female"
-# ans = singlequery(col2,res2) 
-# print(ans)
-# print()
-# new_result = [t for t in result if t in ans]
-# result = new_result
-# print(result)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def guess_faculty():
-# 	criteria = []
-#     if request.form.get('subject_name'):
-#         criteria.append(f"subject_name = '{request.form['subject_name']}'")
-#     if request.form.get('gender'):
-#         criteria.append(f"gender = '{request.form['gender']}'")
-#     if request.form.get('doctorate'):
-#         criteria.append(f"doctorate = '{request.form['doctorate']}'")
-#     if request.form.get('office'):
-#         criteria.append(f"office = '{request.form['office']}'")
-#     if request.form.get('year_of_study'):
-#         criteria.append(f"year_of_study = '{request.form['year_of_study']}'")
-    
-#     result = query_faculty(criteria)
-#     if len(result) == 1:
-#         faculty = result[0]
-#         return render_template('result.html', faculty=faculty)
-#     else:
-#         return render_template('retry.html')
-
-
-# # Simulate user input (example criteria)
-# criteria = []
-# criteria.append("subject_name = 'Computer Engineering'")
-# criteria.append("gender = 'female'")
-# criteria.append("doctorate = 'yes'")
-# criteria.append("office = 'Academic Complex'")
-# criteria.append("year_of_study = 2")
-
-# result = query_faculty(criteria)
-# # if len(result) == 1:
-# # 	faculty = result[0]
-# # 	print("Found Faculty: ",faculty)
-# # else:
-# # 	print("No faculty found.")
-# print(result)
